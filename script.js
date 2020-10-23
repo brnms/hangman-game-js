097601bdaf4318442bca66b844c411e234c4c679
Bruno Santos <bruno.matasantos@gmail.com>
2020-10-23 11:56:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const words = ['application', 'programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['w', 'i', 'z', 'a', 'r', 'd'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // show hidden
@@ -34,5 +34,79 @@ function displayWord() {
         popup.style.display = 'flex';
     }
 }   
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+    // display parts
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    // display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block'
+        }else {
+            part.style.display = 'none'
+        }
+    });
+
+    // Check if not
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost. 😕'
+        popup.style.display = 'flex'
+    }
+}
+
+// show notification
+function showNotification() {
+    notification.classList.add('show')
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 2000)
+}
+
+// keydown letter press
+window.addEventListener('keydown', e => {
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+        if(selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            }else {
+                showNotification()
+            }
+        }else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter)
+
+                updateWrongLettersEl();
+            }else{
+                showNotification()
+            }
+        }
+    }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = 'none'
+});
 
 displayWord()
